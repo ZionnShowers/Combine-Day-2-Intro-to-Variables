c53cb35fa3b30d2a804af21939210ff8016ea8cc
ZionnShowers <zionn.d.showers@gmail.com>
2026-09-09 09:11:08 -0700

    Initial Commit for Day 2 Lecture

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Day Two Variable Lecture
+
+//Console.Clear clears our terminal
+Console.Clear();
+
+//Console.WriteLine Prints out to our terminal / console
+Console.WriteLine("Welcome to Day 2!");
+
+//Console.Write Prints out to our terminal / console without a line break after
+Console.Write("Enter Your Name: ");
+
+//This allows the user to type into the console
+Console.ReadLine();
+//string name = Console.ReadLine();
+
+//What is a Variable
+
+//A Variable is a Box that can store Values.
+//There are several Data Types within C#
+
+//string data types are text between double quotes "";
+//To declare a string Variable, we need to type string before the Variable and then assign it a value.
+string album = "Crush 40"; //We areinitializing a string with a value
+string name; //Declaring a string. We are not giving it a value
+
+//Number Data Types
+//Think of integers as whole numbers
+int age = 18;
+
+//Double can go up to 15-17 digits
+double chiptoleBurrito = 14.45;
+
+//Boolean Data Type represent
+bool isItColdInHere = true; //Camel Case is when you have multiple words in a variable name. First word is lowercase
+
+Console.WriteLine(album);
+
+//Console.WriteLine(name);
